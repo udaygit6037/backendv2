685d7b53df539d9eb4ed6f6cfe6463a40bd26654
Uday Nayak <udaynayak6037@gmail.com>
2025-09-06 16:52:36 +0530

    jwt and refresh token

--- src/modals/users.model.js
+++ src/modals/users.model.js
@@ -1,23 +1,55 @@
 import mongoose from "mongoose";
-
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 const usersSchema = new mongoose.Schema({
   watchHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
-  username: { 
-    type: String, 
+  username: {
+    type: String,
     required: true,
-    unique: true, 
-    lowercase: true, 
-    trim: true, 
-    index: true 
+    unique: true,
+    lowercase: true,
+    trim: true,
+    index: true
   },
   email: { type: String, required: true, unique: true, trim: true },
   fullName: { type: String, required: true, trim: true },
-  password: { type: String, required: true },
+  password: {
+    type: String, required: true
+  },
   avatar: { type: String, default: "" },
   coverImage: { type: String, default: "" },
   refreshToken: { type: String, default: "" }
 }, { timestamps: true });
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+usersSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+usersSchema.methods.generateJWT = function () {
+  return jwt.sign(
+    {
+      id: this._id, username: this.username, email: this.email,
+      fullName: this.fullName
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+};
+usersSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    { id: this._id },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: "7d" }
+  );
+}
 const User = mongoose.model("User", usersSchema);
 
 export default User;
--- src/modals/video.model.js
+++ src/modals/video.model.js
@@ -10,6 +10,6 @@ const videoSchema = new mongoose.Schema({
   views: { type: Number, default: 0 },
   isPublished: { type: Boolean, default: true }
 }, { timestamps: true });
-
+videoSchema.plugin(mongooseAggregatePaginate);
 const Video = mongoose.model("Video", videoSchema);
 export default Video;
